e4d832b189186633f1c5bffdb00a6e819bfeea7f
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-10-10 01:33:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,9 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    doc = Nokogiri::HTML.open()
+    doc = Nokogiri::HTML.open(index_url)
+    
+    
   end
 
   def self.scrape_profile_page(profile_url)
